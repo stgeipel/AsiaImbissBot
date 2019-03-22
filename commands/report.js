ce7064d7334de92f77d13813415b3014d8bc3498
Steffen Geipel <geipel.steffen@gmail.com>
2019-03-22 19:59:12 +0100

    fixing Report problem

--- commands/report.js
+++ commands/report.js
@@ -1,26 +1,22 @@
-const Discord = require("discord.js");
-const fs = require("fs");
-const ms = require("ms");
-const Report = require("../models/mreport.js");
-const mongoose = require("mongoose");
+const Discord = require('discord.js')
+const fs = require('fs')
+const ms = require('ms')
+const Report = require('../models/mreport.js')
+const mongoose = require('mongoose')
 
 module.exports.run = async (bot, message, args) => {
-  if (!message.member.hasPermission("MANAGE_MEMBERS"))
-    return message.reply("Keine Berechtigung dies zu tun");
+  if (!message.member.hasPermission('MANAGE_MEMBERS'))
+    return message.reply('Keine Berechtigung dies zu tun')
   let wUser =
-    message.guild.member(message.mentions.users.first()) ||
-    message.guild.members.get(args[0]);
-  mongoose.connect("mongodb://localhost/soe", { useNewUrlParser: true });
+    message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
+  mongoose.connect('mongodb://localhost/soe', { useNewUrlParser: true })
   //if(!wUser) return message.reply("Couldn't find them yo");
   //if(wUser.hasPermission("MANAGE_MESSAGES")) return message.reply("They waaaay too kewl");
-  let reason = args.join(" ").slice(22);
-  if (reason === "")
+  let reason = args.join(' ').slice(22)
+  if (reason === '')
     return message
-      .reply("Es wurde kein Grund angegben. Bitte geben Sie einen Grund an.")
-      .then(msg => msg.delete(3000));
-  // if(!warns[wUser.id]) warns[wUser.id] = {
-  //   warns: []
-  // };
+      .reply('Es wurde kein Grund angegben. Bitte geben Sie einen Grund an.')
+      .then(msg => msg.delete(3000))
 
   const report = new Report({
     _id: mongoose.Types.ObjectId(),
@@ -31,83 +27,60 @@ module.exports.run = async (bot, message, args) => {
     rUserId: message.author.id,
     time: message.createdAt,
     serverId: message.guild.id
-  });
-
-  let count = 0;
-
+  })
+  let count = 0
   report
     .save()
-    .then(r => {
-      Report.countDocuments(
-        { userId: wUser.user.id, serverId: message.guild.id },
-        function(e, c) {
-          count = c;
-        }
-      );
+    .then(async () => {
+      count = await Report.countDocuments({ userId: wUser.user.id, serverId: message.guild.id })
+      console.log(count)
     })
-    .catch(err => console.log(err));
+    .catch(err => console.log(err))
 
+  //Output for Report Channel
   let warnEmbed = new Discord.RichEmbed()
-    .setDescription("Meldung")
+    .setDescription('Meldung')
     .setAuthor(message.author.username)
-    .setColor("#fc6400")
-    .addField("Melung ausgesprochen gegen", `<@${wUser.id}>`)
-    .addField("wurde gemeldet in", message.channel)
-    .addField("Anzah der Meldungen", count)
-    .addField("Grund", reason);
+    .setColor('#fc6400')
+    .addField('Melung ausgesprochen gegen', `<@${wUser.id}>`)
+    .addField('wurde gemeldet in', message.channel)
+    .addField('Anzah der Meldungen', count)
+    .addField('Grund', reason)
 
-  // warns[wUser.id].warns.push(warnEmbed);
-
-  // fs.writeFile("./configs/warnings.json", JSON.stringify(warns), (err) => {
-  //   if (err) console.log(err)
-  // });
-
-  let warnchannel = message.guild.channels.find(x => x.name == "Reports");
-
-  if (!warnchannel) {
+  let warnchannel = message.guild.channels.find(x => x.name == 'reports')
+  if (!warnchannel && message.member.hasPermission('MANAGE_CHANNELS')) {
     message.channel
-      .send(
-        "Es ist kein Report Channel vorhanden. Soll ich diesen für dich erstellen?"
-      )
-      .then(nm => {
-        nm.react("👍");
-        nm.react("👎");
+      .send('Es ist kein Report Channel vorhanden. Soll ich diesen für dich erstellen?')
+      .then(async nm => {
+        await nm.react('👍')
+        await nm.react('👎')
 
-        // Create a reaction collector
-        const filter = (reaction, user) =>
-          reaction.emoji.name === "👌" && user.id === "someID";
-        const collector = message.createReactionCollector(filter, {
-          time: 15000
-        });
-        collector.on("collect", r => console.log(`Collected ${r.emoji.name}`));
-        collector.on("end", collected =>
-          console.log(`Collected ${collected.size} items`)
-        );
-      });
+        const filter = (reaction, user) => {
+          return ['👍', '👎'].includes(reaction.emoji.name) && user.id === message.author.id
+        }
+
+        nm.awaitReactions(filter, { max: 1, time: 15000, errors: ['time'] })
+          .then(collected => {
+            const reaction = collected.first()
+
+            if (reaction.emoji.name === '👍') {
+              message.guild.createChannel('reports', 'text').then(c => {
+                // warnchannel = message.guild.channels.find(x => x.name == 'reports')
+                c.send(warnEmbed)
+              })
+            }
+          })
+          .catch(collected => {
+            console.log(`After a minute, only ${collected.size} out of 4 reacted.`)
+            message.reply("you didn't react with neither a thumbs up, nor a thumbs down.")
+          })
+      })
   } else {
-    warnchannel.send(warnEmbed);
+    warnchannel.send(warnEmbed)
   }
-
-  // if(warns[wUser.id].warns == 2){
-  //   let muterole = message.guild.roles.find(`name`, "muted");
-  //   if(!muterole) return message.reply("You should create that role dude.");
-
-  //   let mutetime = "10s";
-  //   await(wUser.addRole(muterole.id));
-  //   message.channel.send(`<@${wUser.id}> has been temporarily muted`);
-
-  //   setTimeout(function(){
-  //     wUser.removeRole(muterole.id)
-  //     message.reply(`<@${wUser.id}> has been unmuted.`)
-  //   }, ms(mutetime))
-  // }
-  // if(warns[wUser.id].warns == 3){
-  //   message.guild.member(wUser).ban(reason);
-  //   message.reply(`<@${wUser.id}> has been banned.`)
-  // }
-};
+}
 
 module.exports.help = {
-  name: "report",
-  description: "Meldet einen Benutzer | report @Benutzer Grund"
-};
+  name: 'report',
+  description: 'Meldet einen Benutzer | report @Benutzer Grund'
+}
---
+++ commands/setup.js
@@ -0,0 +1,6 @@
+module.exports.run = async (bot, msg, args) => {}
+
+module.exports.help = {
+  name: 'setup',
+  description: 'Installationsvorgang starten oder wiederholen'
+}
