a2f7d20f017a95c44b39de686dd630e0ae13dd0a
Steffen Geipel <geipel.steffen@gmail.com>
2019-04-01 22:47:07 +0200

    instant

--- index.js
+++ index.js
@@ -1,69 +1,69 @@
-const Discord = require('discord.js');
-const fs = require('fs');
-const Embeds = require('./embed');
-const websocket = require('./WebSocket/websocket');
-const gMemUpdate = require('./events/guildMemberUpdate');
+const Discord = require('discord.js')
+const fs = require('fs')
+const Embeds = require('./embed')
+const websocket = require('./WebSocket/websocket')
+const gMemUpdate = require('./events/guildMemberUpdate')
 
-const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
-var bot = new Discord.Client();
-var mongoose = require('mongoose');
-var ws = new websocket('123', 3000, bot);
+const config = JSON.parse(fs.readFileSync('config.json', 'utf8'))
+var bot = new Discord.Client()
+var mongoose = require('mongoose')
+var ws = new websocket('123', 3000, bot)
 
-bot.commands = new Discord.Collection();
+bot.commands = new Discord.Collection()
 
 fs.readdir('./commands', (err, files) => {
-  if (err) console.log(err);
+  if (err) console.log(err)
 
-  let jsfile = files.filter(f => f.split('.').pop() === 'js');
+  let jsfile = files.filter(f => f.split('.').pop() === 'js')
   if (jsfile.length <= 0) {
-    console.log('Befehle konnten nicht geladen werden');
-    return;
+    console.log('Befehle konnten nicht geladen werden')
+    return
   }
 
   jsfile.forEach((f, i) => {
-    let props = require(`./commands/${f}`);
-    bot.commands.set(props.help.name, props);
-  });
-});
+    let props = require(`./commands/${f}`)
+    bot.commands.set(props.help.name, props)
+  })
+})
 
 // Client thinks
 
 bot.on('ready', () => {
-  console.log(`Logged in as ${bot.user.tag}!`);
+  console.log(`Logged in as ${bot.user.tag}!`)
   // try {
   //   mongoose.connect("mongodb://localhost/" + bot.guil)
   // } catch (error) {
   //   console.log(error)
   // }
-});
+})
 
 bot.on('message', async msg => {
   if (Embeds.checkRights(msg, config.owner)) {
     var cont = msg.content,
       author = msg.member,
       chan = msg.channel,
-      guild = msg.guild;
+      guild = msg.guild
 
     if (msg.channel.type == 'text' && author.id != bot.user.id && cont.startsWith(config.prefix)) {
       var invoke = cont
           .split(' ')[0]
           .substr(config.prefix.length)
           .toLowerCase(),
-        args = cont.split(' ').slice(1);
+        args = cont.split(' ').slice(1)
 
       // if (args.length == 0) Embeds.error(msg, 'Es wurde keine Argumente zu diesem Befehl angegeben')
-      args = args.filter(x => x !== '');
-      let cmdfile = bot.commands.get(invoke);
-      if (cmdfile) cmdfile.run(bot, msg, args);
+      args = args.filter(x => x !== '')
+      let cmdfile = bot.commands.get(invoke)
+      if (cmdfile) cmdfile.run(bot, msg, args)
     }
   }
-});
+})
 
 bot.on('guildMemberUpdate', (mold, mnew) => {
-  gMemUpdate.checkRolesAtUser(mold, mnew);
-});
+  gMemUpdate.checkRolesAtUser(mold, mnew)
+})
 
-bot.login(config.token);
+bot.login(config.token)
 
-module.exports.bot = bot;
-module.exports.mongoose = mongoose;
+module.exports.bot = bot
+module.exports.mongoose = mongoose
